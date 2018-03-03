e5af3c6819ebf576f107f17ba89b6f01671aa465
pisey <pisey@marinax.biz>
2018-03-03 10:10:00 +0700

    - Pagination and delete article.

--- resources/assets/js/components/Articles.vue
+++ resources/assets/js/components/Articles.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
         <h2>Articles</h2>
+        <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li v-bind:class="[{disabled: !pagination.prev_page_url }]" class="page-item"><a class="page-link" href="#" 
+            @click="fetchArticles(pagination.prev_page_url)">Previous</a></li>
+            <li class="page-item disabled"><a class="page-link text-dark" href="#">
+                Page {{ pagination.current_page }} of {{ pagination.last_page }}</a></li>
+
+
+            <!-- <li v-for="page in pagination.last_page" v-bind:key="" class="page-item"><a class="page-link" href="#">2</a></li> -->
+            <!-- <li class="page-item"><a class="page-link" href="#">3</a></li> -->
+            <li v-bind:class="[{disabled: !pagination.next_page_url }]" class="page-item"><a class="page-link" href="#" 
+            @click="fetchArticles(pagination.next_page_url)"> Next</a></li>
+        </ul>
+        </nav>
+        <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
+            <h3>{{ article.title}}</h3>
+            <h3>{{ article.body}}</h3>
+            <hr>
+            <button @click="deleteArticle(article.id)" class="btn btn-danger"> Delete </button>
+
+        </div>
     </div>
 </template>
 <script>
@@ -25,12 +46,39 @@
         },
 
         methods: {
-            fetchArticles(){
-                fetch('api/articles')
+            fetchArticles(page_url){
+                let vm = this;
+                page_url = page_url || '/api/articles';
+                fetch(page_url)
                 .then(res => res.json())
                 .then(res => {
-                    console.log(res.data);
+                    this.articles = res.data;
+                    vm.makePagination(res.meta,res.links);
                 })
+                .catch(err => console.log(err));
+            },
+            makePagination(meta,links){
+                let pagination = {
+                    current_page: meta.current_page,
+                    last_page: meta.last_page,
+                    next_page_url: links.next,
+                    prev_page_url: links.prev
+                }
+                this.pagination = pagination;
+            },
+
+            deleteArticle(id){
+                if(confirm('Are you sure??')){
+                    fetch(`api/article/${id}`,{
+                        method: 'delete'
+                    })
+                    .then(res => res.json()) 
+                    .then(data => {
+                        alert('Article Removed');
+                        this.fetchArticles();
+                    })
+                    .catch(err => console.log(err));
+                }
             }
         }
     }
